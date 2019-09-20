780b8be662dd09ad6f8ee0a8ac83715c56b86ba7
Jack Daniel Kinne <bravelemming@gmail.com>
2019-09-20 13:23:53 -0700

    working backend.

--- src/main/java/lambdaDynamo/Library.java
+++ src/main/java/lambdaDynamo/Library.java
@@ -13,9 +13,14 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 
 public class Library {
@@ -51,9 +56,11 @@ public class Library {
         return tasks;
     }
 
-    public List<Task> getUserTasks(Task task){
+    public APIGatewayProxyResponseEvent getUserTasks(APIGatewayProxyRequestEvent event, Context context){
         HashMap<String, AttributeValue> eav = new HashMap<>();
-        eav.put(":v1", new AttributeValue().withS(task.getAssignee()));
+        Map<String,String> map = event.getPathParameters();
+        String assignee = map.get("user");
+        eav.put(":v1", new AttributeValue().withS(assignee));
         DynamoDBScanExpression scan = new DynamoDBScanExpression()
                 .withFilterExpression("(assignee = :v1)")
                 .withExpressionAttributeValues(eav);
@@ -62,8 +69,38 @@ public class Library {
         DynamoDBMapper ddbMapper = new DynamoDBMapper(ddb);
         List<Task> tasks = ddbMapper.scan(Task.class, scan);
 
-        return tasks;
+        //we are creating our return here
+        APIGatewayProxyResponseEvent res = new APIGatewayProxyResponseEvent();
+
+        //convert to JSON format
+        Gson gson = new Gson();
+        String json = gson.toJson(tasks);
+        //set into the response
+        res.setBody(json);
+
+        return res;
     }
+    //the response body for a return of res looks like this:
+//    [
+//    {
+//        "id": "bdc4ddcd-70e4-4d4f-b06a-9ebac560616c",
+//            "title": "friday afternoon check",
+//            "description": "153 pm second flight",
+//            "status": "assigned",
+//            "assignee": "Jack",
+//            "url": "https://cdn.drawception.com/images/panels/2016/2-13/zhcYmxr64r-2.png",
+//            "historyList": [
+//        {
+//            "date": "Fri Sep 13 20:56:57 UTC 2019",
+//                "action": "assigned"
+//        },
+//        {
+//            "date": "Fri Sep 13 20:56:57 UTC 2019",
+//                "action": "task is assigned to: Jack"
+//        }
+//    ]
+//    }
+//]
 
     public Task deleteTask(Task task){
         final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
